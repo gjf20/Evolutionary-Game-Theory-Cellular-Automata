d38bf1964a7b3abdb403deccee06071a42830d2f
djinn1540 <gregoryjamesflynn@gmail.com>
2018-12-11 01:58:30 -0500

    code compiles and includes framework - next step to use framework in main function

--- directReciprocity.py
+++ directReciprocity.py
@@ -9,96 +9,6 @@ import random
 from enum import Enum
 import inspect
 
-#Set Initial Parameters
-ben = 0.15 #benefit of interacting with a C
-cost = 0.2 #cost of interacting with a D
-b = 0.9 #birth threshold
-d = 0.1 #death threshold
-K = 5 #size of grid ie carrying capacity
-C0 = 5 #labled as 1
-D0 = 5 #labled as 2
-Cfit0 = 0.5 #initial fitness of cell type C
-Dfit0 = 0.5 #initial fitness of cell type D
-Cfit = np.repeat(Cfit0,K)
-Dfit = np.repeat(Dfit0,K)
-init_counts = C0+D0
-grid = np.zeros((K,K))
-
-EMPTY = 0;
-COOPERATOR = 1;
-DEFECTOR = 2;
-
-#Seed C0,D0
-count = 0
-C_ind = [] #ignore for now
-D_ind = []
-ind = []
-while count< init_counts:
-    a = random.randint(0,K-1)
-    b = random.randint(0,K-1)
-    c = random.randint(0,K-1)
-    d = random.randint(0,K-1)
-    if grid[a,b] == 0:
-        grid[a,b] = 1
-        C_ind.append([a,b])
-    else:
-        for i in np.arange(0,K-1):
-            for j in np.arange(0,K-1):
-                if grid[i,j] ==0:
-                    ind.append([i,j])
-        grid[ind[0][0],ind[0][1]] = 1
-        C_ind.append([ind[0][0],ind[0][1]])
-        ind = []
-    count+=1
-
-    if grid[c,d] == 0:
-        grid[c,d] = 2
-        D_ind.append([c,d])
-    else:
-        for i in np.arange(0,K-1):
-            for j in np.arange(0,K-1):
-                if grid[i,j] ==0:
-                    ind.append([i,j])
-        grid[ind[0][0],ind[0][1]] = 2
-        D_ind.append([ind[0][0],ind[0][1]])
-        ind = []
-    count+=1
-print(grid)
-print(C_ind)
-print(D_ind)
-
-#Run CA
-#Check neighbors and updates fit according to payoff matrix, using standard payoff matrix
-count = 0
-while count<K:
-    for i in np.arange(0,K-2):
-        for j in np.arange(0,K-1):
-            if grid[i,j]==1:
-                if grid[i+1,j]==1:
-                    Cfit[count] = Cfit[count]+(ben-cost)
-                    if Cfit[count] < d:
-                        dies = C_ind[count]
-                        grid[dies[0],dies[1]] = 0
-                    elif Cfit[count] > b: #need a check on prolif, cc or stasis threshold
-                        prolif = C_ind[count]
-                        if grid[prolif[0]+1,prolif[1]] == 0 & prolif[0]+1<K-1:  #TODO this condition seems like it is in the wrong order
-                            # TODO why do we make sure not to have prolif[0]+1 = k-1? wont we ignore a row of grid?
-                            grid[prolif[0]+1,prolif[1]] = 1
-                elif grid[i+1,j]==2:
-                    Cfit[count] = Cfit[count]-cost
-                    if Cfit[count] < d:
-                        dies = C_ind[count]
-                        grid[dies[0],dies[1]] = 0
-                    elif Cfit[count] > b: #need a check on prolif, cc or stasis threshold
-                        prolif = C_ind[count]
-                        if grid[prolif[0]+1,prolif[1]] ==0 & prolif[0]+1<K-1: #TODO this condition seems like it is in the wrong order
-                            # TODO why do we make sure not to have prolif[0]+1 = k-1? wont we ignore a row of grid?
-                            grid[prolif[0]+1,prolif[1]] = 1
-                count+=1
-
-
-
-print(grid)
 
 
 def getMooreNeighborhood(self, grid, i, j):
@@ -156,30 +66,161 @@ class Strategy(Enum):
     COOPERATOR = 1
     DEFECTOR = 2
 
+coopC = .25
+coopB = .3
+
+class NeighborDirection(Enum):
+    """The indicies of each direction in the neighbor list"""
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
+    NORTHEAST = 4
+    SOUTHEAST = 5
+    SOUTHWEST = 6
+    NORTHWEST = 7
+
 class Player(object):
     """Player represents a player of the evolution game, it has grid coordinates, fitness level, and a strategy for playing the game"""
 
-    i  #need coordinates to keep track of each pairwise relationship
-    j
-    fitness
-    strat
-
     def __init__(self, i, j, strategy):
-        super([object Object], self).__init__()
-        self.i = i
+
+        self.i = i #may not need these
         self.j = j
         self.fitness = 0
-        self.strat = strat
+        self.strat = strategy
+        self.neighbors = []
+        self.prevOppMoves = np.zeros(8) #just initialize to 8 to cover largest case # list of the previous moves of each neighbor, neighbor direction is index of move in list
+        self.currOppMoves = np.zeros(8) #just initialize to 8 to cover largest case # list of moves of each neighbor in the current grid update
 
-    def updateFitnessWith(self, neighbors):  #where strategy behaviour is executed
-        for n in neighbors:
+    def setNeighbors(self, grid): #must be called after entire grid is initialized
+        self.neighbors = getMooreNeighborhood(grid, self.i, self.j)
+
+    def updateFitnessOwnMove(self):  # 1/2 functions where strategy pays off
+
+        for n in self.neighbors:
             if isinstance(n, Player):
-                if n.strat == Strategy.COOPERATOR:
-                    self.fitness = self.fitness + coopB  #gets the benefit if the neighbor will cooperate
-                else: # strat is DEFECTOR
-                    self.fitness = self.fitness + 0
-
-                if self.strat == Strategy.COOPERATOR:
+                move = self.chooseMoveDirectReciprocity(self.prevOppMoves[self.neighbors.index(n)])
+                if move == Strategy.COOPERATOR:
                     self.fitness = self.fitness - coopC  #loses the cost if this player cooperates
                 else:
                     self.fitness = self.fitness - 0 #no cost if this player defects
+
+                n.currOppMoves[n.neighbors.index(self)] = move
+
+    def updateFitnessDueToOppMoves(self): # should be called after all players have made their currOppMoves - 2/2 functions where strategy pays off
+        for m in self.currOppMoves:
+            if m == 1:
+                self.fitness = self.fitness + coopB # recieves benefit from neighbor cooperating
+            else: #neighbor defected, recieves no benefit
+                self.fitness = self.fitness + 0
+        self.prevOppMoves = self.currOppMoves
+
+
+    def chooseMoveDirectReciprocity(self, opponentLastMove): #returns the strategy for this turn  #where strategy behaviour is executed
+        if self.strat == Strategy.COOPERATOR:
+            if opponentLastMove == 0:
+                return 1 # cooperates
+            else:
+                return opponentLastMove   #basically tit for tat, open to change # TODO:
+        else:
+            if opponentLastMove == 0:
+                return 2 #defects
+            else:
+                return opponentLastMove  #tit for tat with start on defect, open to change:
+
+def main():
+    #Set Initial Parameters
+    ben = 0.15 #benefit of interacting with a C
+    cost = 0.2 #cost of interacting with a D
+    b = 0.9 #birth threshold
+    d = 0.1 #death threshold
+    K = 5 #size of grid ie carrying capacity
+    C0 = 5 #labled as 1
+    D0 = 5 #labled as 2
+    Cfit0 = 0.5 #initial fitness of cell type C
+    Dfit0 = 0.5 #initial fitness of cell type D
+    Cfit = np.repeat(Cfit0,K)
+    Dfit = np.repeat(Dfit0,K)
+    init_counts = C0+D0
+    grid = np.zeros((K,K))
+
+    EMPTY = 0;
+    COOPERATOR = 1;
+    DEFECTOR = 2;
+
+    #Seed C0,D0
+    count = 0
+    C_ind = [] #ignore for now
+    D_ind = []
+    ind = []
+    while count< init_counts:
+        a = random.randint(0,K-1)
+        b = random.randint(0,K-1)
+        c = random.randint(0,K-1)
+        d = random.randint(0,K-1)
+        if grid[a,b] == 0:
+            grid[a,b] = 1
+            C_ind.append([a,b])
+        else:
+            for i in np.arange(0,K-1):
+                for j in np.arange(0,K-1):
+                    if grid[i,j] ==0:
+                        ind.append([i,j])
+            grid[ind[0][0],ind[0][1]] = 1
+            C_ind.append([ind[0][0],ind[0][1]])
+            ind = []
+        count+=1
+
+        if grid[c,d] == 0:
+            grid[c,d] = 2
+            D_ind.append([c,d])
+        else:
+            for i in np.arange(0,K-1):
+                for j in np.arange(0,K-1):
+                    if grid[i,j] ==0:
+                        ind.append([i,j])
+            grid[ind[0][0],ind[0][1]] = 2
+            D_ind.append([ind[0][0],ind[0][1]])
+            ind = []
+        count+=1
+    print(grid)
+    print(C_ind)
+    print(D_ind)
+
+    #Run CA
+    #Check neighbors and updates fit according to payoff matrix, using standard payoff matrix
+    count = 0
+    while count<K:
+        for i in np.arange(0,K-2):
+            for j in np.arange(0,K-1):
+                if grid[i,j]==1:
+                    if grid[i+1,j]==1:
+                        Cfit[count] = Cfit[count]+(ben-cost)
+                        if Cfit[count] < d:
+                            dies = C_ind[count]
+                            grid[dies[0],dies[1]] = 0
+                        elif Cfit[count] > b: #need a check on prolif, cc or stasis threshold
+                            prolif = C_ind[count]
+                            if grid[prolif[0]+1,prolif[1]] == 0 & prolif[0]+1<K-1:  #TODO this condition seems like it is in the wrong order
+                                # TODO why do we make sure not to have prolif[0]+1 = k-1? wont we ignore a row of grid?
+                                grid[prolif[0]+1,prolif[1]] = 1
+                    elif grid[i+1,j]==2:
+                        Cfit[count] = Cfit[count]-cost
+                        if Cfit[count] < d:
+                            dies = C_ind[count]
+                            grid[dies[0],dies[1]] = 0
+                        elif Cfit[count] > b: #need a check on prolif, cc or stasis threshold
+                            prolif = C_ind[count]
+                            if grid[prolif[0]+1,prolif[1]] ==0 & prolif[0]+1<K-1: #TODO this condition seems like it is in the wrong order
+                                # TODO why do we make sure not to have prolif[0]+1 = k-1? wont we ignore a row of grid?
+                                grid[prolif[0]+1,prolif[1]] = 1
+                    count+=1
+
+
+
+    print(grid)
+
+
+if __name__ == "__main__":
+    main()
