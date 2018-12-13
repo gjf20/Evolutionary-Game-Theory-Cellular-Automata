6f31389585439a323f0ecbd1dfbacc02dce2983f
djinn1540 <gregoryjamesflynn@gmail.com>
2018-12-12 23:36:16 -0500

    proper birthing

--- directReciprocity.py
+++ directReciprocity.py
@@ -104,10 +104,10 @@ class Player(object):
         for n in self.neighbors:
             if isinstance(n, Player):
                 move = self.chooseMoveDirectReciprocity(self.prevOppMoves[self.neighbors.index(n)])
-                if move == Strategy.COOPERATOR:
+                if move == Strategy.COOPERATOR.value:
                     self.fitness = self.fitness - coopC  #loses the cost if this player cooperates
                 else:
-                    self.fitness = self.fitness - 0 #no cost if this player defects
+                    self.fitness = self.fitness #no cost if this player defects
                 n.currOppMoves[n.neighbors.index(self)] = move
 
     def updateFitnessDueToOppMoves(self): # should be called after all players have made their currOppMoves - 2/2 functions where strategy pays off
@@ -123,15 +123,21 @@ class Player(object):
 
     def chooseMoveDirectReciprocity(self, opponentLastMove): #returns the strategy for this turn  #where strategy behaviour is executed
         if self.strat == Strategy.COOPERATOR:
+            return 1
+            '''
             if opponentLastMove == 0:
                 return 1 # cooperates
             else:
                 return opponentLastMove   #basically tit for tat, open to change # TODO:
+            '''
         else:
+            return 2
+            '''
             if opponentLastMove == 0:
                 return 2 #defects
             else:
                 return opponentLastMove  #tit for tat with start on defect, open to change:
+            '''
 
     def shouldDie(self):
         return self.fitness < d
@@ -176,44 +182,50 @@ class Player(object):
 
     def __repr__(self):
         if self.strat == Strategy.COOPERATOR:
-            return str(1)
+            return '   ' + str(1)
         else: #DEFECTOR
-            return str(2)
+            return '   ' + str(2)
 
     def __str__(self):
         if self.strat == Strategy.COOPERATOR:
-            return str(1)
+            return  str(1)
         else: #DEFECTOR
-            return str(2)
+            return  str(2)
 
 def printGrid(grid):
     for row in grid:
         print(row)
 
+def printFitnessGrid(grid):
+    for i in np.arange(0, len(grid)):
+        line = ' '
+        for j in np.arange(0, len(grid[i])):
+            if grid[i][j] is not None:
+                line = line + f'{grid[i][j].fitness:.2f}' + '  '
+            else:
+                line = line + '----  '
+        print(line)
+
+
 def main():
     #Set Initial Parameters
-    ben = 0.15 #benefit of interacting with a C
-    cost = 0.2 #cost of interacting with a D
-    b = 0.9 #birth threshold
-    d = 0.1 #death threshold
+
     K = 5 #size of grid ie carrying capacity
     C0 = 5 #labled as 1
     D0 = 5 #labled as 2
     Cfit0 = 0.5 #initial fitness of cell type C
     Dfit0 = 0.5 #initial fitness of cell type D
-    Cfit = np.repeat(Cfit0,K)
-    Dfit = np.repeat(Dfit0,K)
     init_counts = C0+D0
     grid = [[None for s in range(K)] for t in range(K)]
 
-    #success counters?
+
+    #success counters
     CoopBirths = 0
     DefectBirths = 0
+    CoopDeaths = 0
+    DefectDeaths = 0
 
-    #Seed C0,D0
     count = 0
-    C_ind = [] #ignore for now
-    D_ind = []
     ind = []
     while count < init_counts:
         a = random.randint(0,K-1)
@@ -222,73 +234,83 @@ def main():
         d = random.randint(0,K-1)
         if grid[a][b] is None:
             grid[a][b] = Player(a,b,Strategy.COOPERATOR)
-            C_ind.append(grid[a][b])
         else:
             for i in np.arange(0,K):
                 for j in np.arange(0,K):
                     if grid[i][j] is None:
                         ind.append([i,j])
             grid[ind[0][0]][ind[0][1]] = Player(ind[0][0],ind[0][1],Strategy.COOPERATOR)
-            C_ind.append(grid[ind[0][0]][ind[0][1]])
             ind = []
         count+=1
 
         if grid[c][d] is None:
             grid[c][d] = Player(c,d,Strategy.DEFECTOR)
-            D_ind.append(grid[c][d])
         else:
             for i in np.arange(0,K):
                 for j in np.arange(0,K):
                     if grid[i][j] is None:
                         ind.append([i,j])
             grid[ind[0][0]][ind[0][1]] = Player(ind[0][0],ind[0][1],Strategy.DEFECTOR)
-            D_ind.append(grid[ind[0][0]][ind[0][1]])
             ind = []
         count+=1
 
+    print('Initial population: ')
     printGrid(grid)
+    print('\n Corresponding Fitness:')
+    printFitnessGrid(grid)
     print(' ')
-    print(C_ind)
-    print(D_ind)
 
+    b = 0.9 #birth threshold
+    d = 0.0 #death threshold
     #Run CA
     #Check neighbors and updates fit according to payoff matrix, using standard payoff matrix
-    timesteps = 1
-    for i in np.arange(0, K):
-        for j in np.arange(0, K):
-            if grid[i][j] is not None:
-                # prepare all neighbors
-                grid[i][j].setNeighbors(grid)
-    for i in np.arange(0, K):
-        for j in np.arange(0, K):
-            if grid[i][j] is not None:
-                # this is where we update fitness based on own moves this turn
-                grid[i][j].updateFitnessOwnMove()
-    for i in np.arange(0, K):
-        for j in np.arange(0, K):
-            if grid[i][j] is not None:
-                # this is where we update fitness based on neighbors' moves this turn
-                grid[i][j].updateFitnessDueToOppMoves()
-    for i in np.arange(0, K):
-        for j in np.arange(0, K):
-            if grid[i][j] is not None:
+    timesteps = 5
+    while timesteps > 0:
+        for i in np.arange(0, K):
+            for j in np.arange(0, K):
+                if grid[i][j] is not None:
+                    # prepare all neighbors
+                    grid[i][j].setNeighbors(grid)
+        for i in np.arange(0, K):
+            for j in np.arange(0, K):
+                if grid[i][j] is not None:
+                    # this is where we update fitness based on own moves this turn
+                    grid[i][j].updateFitnessOwnMove()
+        for i in np.arange(0, K):
+            for j in np.arange(0, K):
+                if grid[i][j] is not None:
+                    # this is where we update fitness based on neighbors' moves this turn
+                    grid[i][j].updateFitnessDueToOppMoves()
+        for i in np.arange(0, K):
+            for j in np.arange(0, K):
+                if grid[i][j] is not None:
 
-                if grid[i][j].shouldDie():  #will defectors ever die?
-                    grid[i][j] = None
-                if grid[i][j].shouldGiveBirth():
-                    birthCoord = grid[i][j].getRandomEmptyNeighbor(grid)
-                    if birthCoord != [-1,-1]:
-                        grid[birthCoord[0]][birthCoord[1]] = Player(birthCoord[0], birthCoord[1], grid[i][j].strat)
-                        grid[i][j].fitness = grid[i][j].fitness - b
+                    if grid[i][j].shouldDie():  #will defectors ever die?
                         if grid[i][j].strat == Strategy.COOPERATOR:
-                            CoopBirths = CoopBirths + 1
+                            CoopDeaths += 1
                         else:
-                            DefectBirths = DefectBirths + 1
-    timesteps -=1
+                            DefectDeaths += 1
+                        grid[i][j] = None
+                    if grid[i][j] is not None and grid[i][j].shouldGiveBirth():
+                        birthCoord = grid[i][j].getRandomEmptyNeighbor(grid)
+                        if birthCoord != [-1,-1]:
+                            grid[birthCoord[0]][birthCoord[1]] = Player(birthCoord[0], birthCoord[1], grid[i][j].strat)
+                            grid[i][j].fitness = grid[i][j].fitness - b
+                            if grid[i][j].strat == Strategy.COOPERATOR:
+                                CoopBirths = CoopBirths + 1
+                            else:
+                                DefectBirths = DefectBirths + 1
+        timesteps -=1
+    print('Final Population: ')
+    printGrid(grid)
+    print('Corresponding Fitness: ')
+    printFitnessGrid(grid)
 
+    print(f'COOPERATOR Births: {CoopBirths}')
+    print(f'DEFECTOR Births: {DefectBirths}')
+    print(f'COOPERATOR Deaths: {CoopDeaths}')
+    print(f'DEFECTOR Deaths: {DefectDeaths}')
 
-
-    print(grid)
 
 
 if __name__ == "__main__":
