6417f80e3e4c436a850597afd7087047cad03142
djinn1540 <gregoryjamesflynn@gmail.com>
2018-12-12 19:50:22 -0500

    works up until ownfitnessmove, weird silent error that i,j mostly do not match the player i,j

--- directReciprocity.py
+++ directReciprocity.py
@@ -11,53 +11,53 @@ import inspect
 
 
 
-def getMooreNeighborhood(self, grid, i, j):
+def getMooreNeighborhood(grid, i, j):
     neighbors = getVonNeumannNeighborhood(grid, i, j)
 
     if i-1 < 0 or j+1 >= len(grid[i-1]): #use of grid[i-1] will avoid error in case of a jagged 2d array
-        neighbors.append(self.EMPTY)
+        neighbors.append(None)
     else:
-        neighbors.append(grid[i-1,j+1]) #Northeast
+        neighbors.append(grid[i-1][j+1]) #Northeast
 
     if i+1 >= len(grid) or j+1 >= len(grid[i+1]): #use of grid[i+1] will avoid error in case of a jagged 2d array
-        neighbors.append(self.EMPTY)
+        neighbors.append(None)
     else:
-        neighbors.append(grid[i+1,j+1]) #Southeast
+        neighbors.append(grid[i+1][j+1]) #Southeast
 
     if i+1 >= len(grid) or j-1 < 0:
-        neighbors.append(self.EMPTY)
+        neighbors.append(None)
     else:
-        neighbors.append(grid[i+1,j-1]) #Southwest
+        neighbors.append(grid[i+1][j-1]) #Southwest
 
     if i-1 < 0 or j-1 < 0: #use of grid[i-1] will avoid error in case of a jagged 2d array
-        neighbors.append(self.EMPTY)
+        neighbors.append(None)
     else:
-        neighbors.append(grid[i-1,j-1]) #Northwest
+        neighbors.append(grid[i-1][j-1]) #Northwest
 
     return neighbors
 
 
-def getVonNeumannNeighborhood(self, grid, i, j):
+def getVonNeumannNeighborhood(grid, i, j):
     neighbors = []
     if i-1 < 0:
-        neighbors.append(self.EMPTY)
+        neighbors.append(None)
     else:
-        neighbors.append(grid[i-1,j]) #North
+        neighbors.append(grid[i-1][j]) #North
 
     if j+1 >= len(grid[i]):
-        neighbors.append(self.EMPTY)
+        neighbors.append(None)
     else:
-        neighbors.append(grid[i, j+1]) #East
+        neighbors.append(grid[i][j+1]) #East
 
     if i+1 >= len(grid):
-        neighbors.append(self.EMPTY)
+        neighbors.append(None)
     else:
-        neighbors.append(grid[i+1,j]) #South
+        neighbors.append(grid[i+1][j]) #South
 
     if j-1 < 0:
-        neighbors.append(self.EMPTY)
+        neighbors.append(None)
     else:
-        neighbors.append(grid[i, j-1]) #West
+        neighbors.append(grid[i][j-1]) #West
 
     return neighbors
 
@@ -67,10 +67,10 @@ class Strategy(Enum):
     DEFECTOR = 2
 
 
-
-
-coopC = .25
-coopB = .3
+b = 0.9 #birth threshold
+d = 0.0 #death threshold
+coopC = .05
+coopB = .1
 
 class NeighborDirection(Enum):
     """The indicies of each direction in the neighbor list"""
@@ -90,11 +90,11 @@ class Player(object):
 
         self.i = i #may not need these
         self.j = j
-        self.fitness = 0
+        self.fitness = 0.45
         self.strat = strategy
         self.neighbors = []
         self.prevOppMoves = np.zeros(8) #just initialize to 8 to cover largest case # list of the previous moves of each neighbor, neighbor direction is index of move in list
-        self.currOppMoves = np.zeros(8) #just initialize to 8 to cover largest case # list of moves of each neighbor in the current grid update
+        self.currOppMoves = np.zeros(8)#just initialize to 8 to cover largest case # list of moves of each neighbor in the current grid update
 
     def setNeighbors(self, grid): #must be called after entire grid is initialized
         self.neighbors = getMooreNeighborhood(grid, self.i, self.j)
@@ -112,11 +112,13 @@ class Player(object):
                 n.currOppMoves[n.neighbors.index(self)] = move
 
     def updateFitnessDueToOppMoves(self): # should be called after all players have made their currOppMoves - 2/2 functions where strategy pays off
-        for m in self.currOppMoves:
-            if m == 1:
-                self.fitness = self.fitness + coopB # recieves benefit from neighbor cooperating
-            else: #neighbor defected, recieves no benefit
-                self.fitness = self.fitness + 0
+        for n in self.neighbors:
+            if isinstance(n, Player):
+                m = self.currOppMoves[self.neighbors.index[n]]
+                if m == 1:
+                    self.fitness = self.fitness + coopB # recieves benefit from neighbor cooperating
+                else: #neighbor defected, recieves no benefit
+                    self.fitness = self.fitness + 0
         self.prevOppMoves = self.currOppMoves
 
 
@@ -132,6 +134,47 @@ class Player(object):
             else:
                 return opponentLastMove  #tit for tat with start on defect, open to change:
 
+    def shouldDie(self):
+        return self.fitness < d
+
+    def shouldGiveBirth(self):
+        return self.fitness > b
+
+    def getRandomEmptyNeighbor(self, grid):
+        count = random.ranint(1, len(self.neighbors)+1)
+        origCount = count
+        ind = 0
+        tries = 0
+        while count > 0:
+
+            if ind == NeighborDirection.NORTH:
+                res = [self.i-1, self.j]
+            elif ind == NeighborDirection.EAST:
+                res =  [self.i, self.j + 1]
+            elif ind == NeighborDirection.SOUTH:
+                res =  [self.i+1, self.j]
+            elif ind == NeighborDirection.WEST:
+                res = [self.i, self.j-1]
+            elif ind == NeighborDirection.NORTHEAST:
+                res =  [self.i-1, self.j+1]
+            elif ind == NeighborDirection.SOUTHEAST:
+                res =  [self.i+1, self.j+1]
+            elif ind == NeighborDirection.SOUTHWEST:
+                res = [self.i+1, self.j-1]
+            else: # direction is NORTHWEST
+                res = [self.i-1, self.j-1]
+
+            n = self.neighbors[ind]
+            if n is None and not (res[0] < 0 or res[1] < 0 or res[0] >= len(grid) or res[1] >= len(grid[res[0]])):
+                count = count - 1
+            ind = (ind + 1) % len(self.neighbors)
+            tries = tries + 1
+
+            if tries > origCount * len(self.neighbors): # this will occur when there are no valid spots to reproduce
+                return [-1,-1]
+        return res
+
+
     def __repr__(self):
         if self.strat == Strategy.COOPERATOR:
             return str(1)
@@ -143,6 +186,10 @@ class Player(object):
             return str(1)
         else: #DEFECTOR
             return str(2)
+
+def printGrid(grid):
+    for row in grid:
+        print(row)
 
 def main():
     #Set Initial Parameters
@@ -158,7 +205,7 @@ def main():
     Cfit = np.repeat(Cfit0,K)
     Dfit = np.repeat(Dfit0,K)
     init_counts = C0+D0
-    grid = np.zeros((K,K))
+    grid = [[None for s in range(K)] for t in range(K)]
 
     #success counters?
     CoopBirths = 0
@@ -169,68 +216,81 @@ def main():
     C_ind = [] #ignore for now
     D_ind = []
     ind = []
-    while count< init_counts:
+    while count < init_counts:
         a = random.randint(0,K-1)
         b = random.randint(0,K-1)
         c = random.randint(0,K-1)
         d = random.randint(0,K-1)
-        if grid[a,b] == 0:
-            grid[a,b] = Player(a,b,Strategy.COOPERATOR)
-            C_ind.append(grid[a,b])
+        if grid[a][b] is None:
+            grid[a][b] = Player(a,b,Strategy.COOPERATOR)
+            C_ind.append(grid[a][b])
         else:
-            for i in np.arange(0,K-1):
-                for j in np.arange(0,K-1):
-                    if grid[i,j] ==0:
+            for i in np.arange(0,K):
+                for j in np.arange(0,K):
+                    if grid[i][j] is None:
                         ind.append([i,j])
-            grid[ind[0][0],ind[0][1]] = Player(a,b,Strategy.COOPERATOR)
-            C_ind.append(grid[ind[0][0],ind[0][1]])
+            grid[ind[0][0]][ind[0][1]] = Player(a,b,Strategy.COOPERATOR)
+            C_ind.append(grid[ind[0][0]][ind[0][1]])
             ind = []
         count+=1
 
-        if grid[c,d] == 0:
-            grid[c,d] = Player(c,d,Strategy.DEFECTOR)
-            D_ind.append(grid[c,d])
+        if grid[c][d] is None:
+            grid[c][d] = Player(c,d,Strategy.DEFECTOR)
+            D_ind.append(grid[c][d])
         else:
-            for i in np.arange(0,K-1):
-                for j in np.arange(0,K-1):
-                    if grid[i,j] ==0:
+            for i in np.arange(0,K):
+                for j in np.arange(0,K):
+                    if grid[i][j] is None:
                         ind.append([i,j])
-            grid[ind[0][0],ind[0][1]] = Player(c,d,Strategy.DEFECTOR)
-            D_ind.append(grid[ind[0][0],ind[0][1]])
+            grid[ind[0][0]][ind[0][1]] = Player(c,d,Strategy.DEFECTOR)
+            D_ind.append(grid[ind[0][0]][ind[0][1]])
             ind = []
         count+=1
-    print(grid)
+
+    printGrid(grid)
+    print(' ')
     print(C_ind)
     print(D_ind)
 
     #Run CA
     #Check neighbors and updates fit according to payoff matrix, using standard payoff matrix
-    count = 0
-    while count<K:
-        for i in np.arange(0,K-2):
-            for j in np.arange(0,K-1):
-                if grid[i,j]==1:
-                    if grid[i+1,j]==1:
-                        Cfit[count] = Cfit[count]+(ben-cost)
-                        if Cfit[count] < d:
-                            dies = C_ind[count]
-                            grid[dies[0],dies[1]] = 0
-                        elif Cfit[count] > b: #need a check on prolif, cc or stasis threshold
-                            prolif = C_ind[count]
-                            if grid[prolif[0]+1,prolif[1]] == 0 & prolif[0]+1<K-1:  #TODO this condition seems like it is in the wrong order
-                                # TODO why do we make sure not to have prolif[0]+1 = k-1? wont we ignore a row of grid?
-                                grid[prolif[0]+1,prolif[1]] = 1
-                    elif grid[i+1,j]==2:
-                        Cfit[count] = Cfit[count]-cost
-                        if Cfit[count] < d:
-                            dies = C_ind[count]
-                            grid[dies[0],dies[1]] = 0
-                        elif Cfit[count] > b: #need a check on prolif, cc or stasis threshold
-                            prolif = C_ind[count]
-                            if grid[prolif[0]+1,prolif[1]] ==0 & prolif[0]+1<K-1: #TODO this condition seems like it is in the wrong order
-                                # TODO why do we make sure not to have prolif[0]+1 = k-1? wont we ignore a row of grid?
-                                grid[prolif[0]+1,prolif[1]] = 1
-                    count+=1
+    timesteps = 1
+    for i in np.arange(0, K):
+        for j in np.arange(0, K):
+            if grid[i][j] is not None:
+                # prepare all neighbors
+                grid[i][j].setNeighbors(grid)
+    for i in np.arange(0, K):
+        for j in np.arange(0, K):
+            if grid[i][j] is not None:
+                # this is where we update fitness based on own moves this turn
+                print(grid[i][j].j)
+                print(grid[i][j].i)
+                print(grid[i][j])
+                print(i)
+                print(j)
+                grid[i][j].updateFitnessOwnMove()
+    for i in np.arange(0, K):
+        for j in np.arange(0, K):
+            if grid[i][j] is not None:
+                # this is where we update fitness based on neighbors' moves this turn
+                grid[i][j].updateFitnessDueToOppMoves()
+    for i in np.arange(0, K):
+        for j in np.arange(0, K):
+            if grid[i][j] is not None:
+
+                if grid[i][j].shouldDie():  #will defectors ever die?
+                    grid[i][j] = None
+                if grid[i][j].shouldGiveBirth():
+                    birthCoord = grid[i][j].getRandomEmptyNeighbor(grid)
+                    if birthCoord != [-1,-1]:
+                        grid[birthCoord[0]][birthCoord[1]] = Player(birthCoord[0], birthCoord[1], grid[i][j].strat)
+                        grid[i][j].fitness = grid[i][j].fitness - b
+                        if grid[i][j].strat == Strategy.COOPERATOR:
+                            CoopBirths = CoopBirths + 1
+                        else:
+                            DefectBirths = DefectBirths + 1
+    timesteps -=1
 
 
 
